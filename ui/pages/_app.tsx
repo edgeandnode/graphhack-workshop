4c1977187d1651c77f150d2e80a7d1b597714fa4
hasparus <hasparus@gmail.com>
2022-05-06 17:55:41 +0200

    Add wagmi and theme-ui

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createClient, Provider } from "wagmi";
+import { ThemeProvider } from "theme-ui";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { theme } from "../src/theme";
+
+import "../styles/globals.css";
+
+const client = createClient();
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider client={client}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
---
+++ ui/src/theme.ts
@@ -0,0 +1,19 @@
+import { makeTheme } from "@theme-ui/css/utils";
+
+export const theme = makeTheme({
+  colors: {
+    primary: "#03e",
+    background: "#fff",
+  },
+  buttons: {
+    primary: {
+      cursor: "pointer",
+    },
+  },
+  layout: {
+    container: {
+      p: 3,
+      maxWidth: "960px",
+    },
+  },
+});
