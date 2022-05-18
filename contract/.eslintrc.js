18ae523cdb92aa0abbdae9420ed0895ae0b6cc8e
hasparus <hasparus@gmail.com>
2022-05-18 16:38:45 +0200

    Fix .eslintrc to get rid of false positives

--- contract/.eslintrc.js
+++ contract/.eslintrc.js
@@ -6,12 +6,20 @@ module.exports = {
     node: true,
   },
   plugins: ['@typescript-eslint'],
-  extends: ['standard', 'plugin:prettier/recommended', 'plugin:n/recommended'],
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:n/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    'n/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+    'n/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'], version: '>=14.0.0' }],
+    'n/no-missing-import': 'off', // false positives, TS checks this
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
   },
 }
