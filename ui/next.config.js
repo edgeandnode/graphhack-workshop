7a9e28c9c7e40a1e249a3b64383d191c5bf956cb
hasparus <hasparus@gmail.com>
2022-05-23 16:20:04 +0200

    Run pnpm build on CI

--- ui/next.config.js
+++ ui/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     SUBGRAPH_URL: process.env.SUBGRAPH_URL,
     INFURA_KEY: process.env.INFURA_KEY,
   },
+  eslint: {
+    ignoreDuringBuilds: true, // we run ESLint from the root directory
+  },
 }
 
 module.exports = nextConfig
